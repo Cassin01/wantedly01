e3af632d78e051f1cf9bfb8c5dd30c0f9342b9c7
Cassin01 <koyojb@gmail.com>
2019-04-13 18:28:53 +0900

    task01

---
+++ Task01/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+  "fmt"
+)
+
+
+func jsonResponse(rw http.ResponseWriter, req *http.Request) {
+  response := []byte(`
+      {
+        "message": "Hello World!!",
+      }
+  `)
+
+  defer func() {
+      rw.Header().Set("Content-Type", "application/json")
+      fmt.Fprint(rw, string(response))
+  }()
+}
+
+func main() {
+  http.HandleFunc("/", jsonResponse)
+  http.ListenAndServe(":8080", nil)
+}
